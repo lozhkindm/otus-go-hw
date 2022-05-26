40d0353b7a3c41abfdc8da37a9b8c5bf62be5b20
Dmitriy <lozhkindm@yandex.ru>
2022-05-26 09:19:36 +0300

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) < 1 {
+		return 1
+	}
+	name, args := cmd[0], cmd[1:]
+	comm := exec.Command(name, args...)
+	envsToSet, envsToDelete := env.GetEnvs()
+	for k, v := range envsToSet {
+		if err := os.Setenv(k, v); err != nil {
+			return 1
+		}
+	}
+	for _, v := range envsToDelete {
+		if _, ok := os.LookupEnv(v); ok {
+			if err := os.Unsetenv(v); err != nil {
+				return 1
+			}
+		}
+	}
+	comm.Stdout, comm.Stdin, comm.Stderr = os.Stdout, os.Stdin, os.Stderr
+	if err := comm.Run(); err != nil {
+		return 1
+	}
+	return 0
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		log.Fatal("at least 3 args required")
+	}
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(RunCmd(os.Args[2:], env))
 }
