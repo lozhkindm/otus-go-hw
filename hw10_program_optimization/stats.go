71dfa46ad7eb97563840de844381308a97ea9c88
Dmitriy <lozhkindm@yandex.ru>
2022-06-08 10:11:30 +0300

    HW10 is completed

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,12 +1,12 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
-	"fmt"
+	"bufio"
+	"errors"
 	"io"
-	"io/ioutil"
-	"regexp"
 	"strings"
+
+	jsoniter "github.com/json-iterator/go"
 )
 
 type User struct {
@@ -19,49 +19,55 @@ type User struct {
 	Address  string
 }
 
+// UserEmail we don't need every field for parsing email addresses.
+type UserEmail struct {
+	Email string
+}
+
 type DomainStat map[string]int
 
+var errInvalidEmail = errors.New("invalid email")
+
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
+	var (
+		scanner = bufio.NewScanner(r)
+		json    = jsoniter.ConfigFastest
+		stat    = make(DomainStat)
+		user    UserEmail
+		dmn     string
+		err     error
+	)
+	for scanner.Scan() {
+		user.Email = ""
+		if err := json.Unmarshal(scanner.Bytes(), &user); err != nil {
 			return nil, err
 		}
-
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		if strings.HasSuffix(user.Email, domain) {
+			if dmn, err = extractDomainFromEmail(user.Email); err != nil {
+				return nil, err
+			}
+			stat[dmn]++
 		}
 	}
-	return result, nil
+	return stat, nil
+}
+
+func extractDomainFromEmail(email string) (string, error) {
+	var (
+		builder  strings.Builder
+		inDomain = false
+	)
+	for _, r := range email {
+		if r == '@' && inDomain {
+			return "", errInvalidEmail
+		}
+		if r == '@' {
+			inDomain = true
+			continue
+		}
+		if inDomain {
+			builder.WriteRune(r)
+		}
+	}
+	return strings.ToLower(builder.String()), nil
 }
